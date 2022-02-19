0847cf08123c3c03164610ed179699cb696ed8df
Anangbril <89025118+Anangbril@users.noreply.github.com>
2022-02-19 13:46:40 +0700

    path variable & query string

--- main.go
+++ main.go
@@ -9,10 +9,11 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/", routerHandler)
-
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler) //this is path variable
+	router.GET("/query", queryHandler)            //this is query string
 
-	router.Run(":8081")
+	router.Run()
 }
 
 func routerHandler(c *gin.Context) {
@@ -25,5 +26,23 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"nama":          "LM. ANANG BRILYANSYAH",
 		"Jenis Kelamin": "Pria",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"price": price,
 	})
 }
